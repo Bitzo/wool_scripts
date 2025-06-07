39c8b3cfac61c1d83a955ed6900444b07ab3d148
YLong Shi <bitzo@qq.com>
2025-06-07 20:09:12 +0800

    Update lawson.js

--- Scripts/wechat/lawson.js
+++ Scripts/wechat/lawson.js
@@ -1,51 +1,50 @@
 let url = $request.url;
 let body = $response.body;
 let resp_data = JSON.parse(body);
-
 try {
-    console.log('request:', $request);
-    console.log('response:', $response);
-    console.log('当前请求URL:', url);
-    console.log('响应数据:', resp_data);
-
     // 处理弹窗和悬浮广告
-    if (url.includes('/portal/app/globalLaunch/listAdvert?')) {
-        if (resp_data.header?.data) {
-            console.log('去除弹窗、悬浮广告');
-            resp_data.header.data = {};
+    if (url.includes("/portal/app/globalLaunch/listAdvert?")) {
+        console.log('去除弹窗、悬浮广告');
+        if (resp_data?.data) {
+            console.log('【已处理】去除弹窗、悬浮广告');
+            resp_data.data = {};
         }
     }
 
     // 处理预购列表
-    if (url.includes('/app/v1/home/getReservation/?')) {
-        if (resp_data.header?.data) {
-            console.log('去除预购列表');
-            resp_data.header.data = {};
+    if (url.includes("/app/v1/home/getReservation/?")) {
+        console.log('去除预购列表');
+        if (resp_data?.data) {
+            console.log('【已处理】去除预购列表');
+            resp_data.data = {};
         }
     }
 
     // 处理推荐列表
-    if (url.includes('/app/v1/home/getRecommendations/?')) {
-        if (resp_data.header?.data) {
-            console.log('去除推荐列表');
-            resp_data.header.data = {};
+    if (url.includes("/app/v1/home/getRecommendations/?")) {
+        console.log('去除推荐列表');
+        if (resp_data?.data) {
+            console.log('【已处理】去除推荐列表');
+            resp_data.data = {};
         }
     }
 
     // 处理首页banner推广
-    if (url.includes('/app/v1/home/getConfigInfo/?')) {
-        if (resp_data.header?.data?.data?.dysmorphismPictureList) {
-            console.log('去除首页banner推广');
-            resp_data.header.data.data.dysmorphismPictureList = [];
+    if (url.includes("/app/v1/home/getConfigInfo/?")) {
+        console.log('去除首页banner推广');
+        if (resp_data?.data?.dysmorphismPictureList) {
+            console.log('【已处理】去除首页banner推广');
+            resp_data.data.dysmorphismPictureList = [];
         }
     }
 
     // 处理首页栏目
-    if (url.includes('/app/v1/mina/systemSetting/?')) {
-        if (resp_data.header?.data?.data) {
-            resp_data.header.data.data = resp_data.header.data.data.map(item => {
+    if (url.includes("/app/v1/mina/systemSetting?")) {
+        console.log('去除首页栏目');    
+        if (resp_data?.data) {
+            resp_data.data = resp_data.data.map(item => {
                 if (item.type === 'HOMETAB') {
-                    console.log('去除首页栏目');
+                    console.log('【已处理】去除首页栏目');
                     item.openFlg = false;
                     item.typeValue = {};
                 }
